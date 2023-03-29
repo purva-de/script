8f6e4abbe719e57c5becf6fea21e0f9878c18c25
purva <purvasemail@gmail.com>
2023-03-29 23:15:06 +0530

    work

---
+++ WD/appp.js
@@ -0,0 +1,49 @@
+
+var products = {
+    1: {
+      name : "KIWI ONION",
+      price : 250
+    },
+    2: {
+      name : "MUSHROOM MALAI",
+      price : 300
+    },
+    3:{
+        name : "TANDOORI",
+        price : 280
+   },
+   4:{
+       name : "PYAZ PANEER",
+       price : 345
+   },
+   5:{
+       name :"CHEESE HI CHEESE ",
+       price : 380
+   },
+   6:{
+    name :"CHATPATA CHICKEN  ",
+    price : 450
+},
+7:{
+    name :"CHICKEN KABAB ",
+    price : 265
+},
+8:{
+    name :"CHICKEN 2 PYAZ ",
+    price : 540
+},
+9:{
+    name :"CHICKEN PANEER ",
+    price : 330
+},
+10:{
+    name :"CHUNKY CHICKEN",
+    price : 620
+},
+}
+
+
+function bill(){
+    var r; 
+    document.write("add " +add);
+}
---
+++ WD/project.js
@@ -0,0 +1,22 @@
+
+        <script type="text/javascript">  
+            function selects(){  
+                var ele=document.getElementsByName('chk');  
+                for(var i=0; i<ele.length; i++){  
+                    if(ele[i].type=='checkbox')  
+                        ele[i].checked=true;  
+                }  
+            }  
+            function deSelect(){  
+                var ele=document.getElementsByName('chk');  
+                for(var i=0; i<ele.length; i++){  
+                    if(ele[i].type=='checkbox')  
+                        ele[i].checked=false;  
+                        ele[i].checked=true;
+                        ele[i]  
+                      
+                }  
+            }             
+        </script>  
+   
+ 
